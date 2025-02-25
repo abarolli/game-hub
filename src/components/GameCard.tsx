d62ec4b7b69e6266e1ef6e7557c1090957ad5ea1
abarolli <obarolli@gmail.com>
2025-02-24 23:21:09 -0700

    Added GameCard.tsx

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,40 @@
+import { Badge, Card, HStack, Image } from "@chakra-ui/react";
+import React from "react";
+import { IconType } from "react-icons";
+
+export interface Platform {
+  name: string;
+  icon: IconType;
+}
+
+interface Props {
+  imgSrc: string;
+  gameTitle: string;
+  criticScore: number;
+  platforms: Platform[];
+}
+
+function GameCard({ gameTitle, imgSrc, criticScore, platforms }: Props) {
+  return (
+    <Card.Root maxW="sm">
+      <Image src={imgSrc} alt={gameTitle} />
+      <Card.Body>
+        <HStack mb="20px" justifyContent="space-between">
+          <HStack>
+            {platforms.map((platform) => (
+              <platform.icon />
+            ))}
+          </HStack>
+          <Badge w="fit" paddingX="10px" colorPalette="green">
+            {criticScore}
+          </Badge>
+        </HStack>
+        <Card.Title fontWeight="bolder" fontSize="1.5rem">
+          {gameTitle}
+        </Card.Title>
+      </Card.Body>
+    </Card.Root>
+  );
+}
+
+export default GameCard;
