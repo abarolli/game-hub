db6efb2f2779c6f74a7d31381514d88a57cde445
abarolli <obarolli@gmail.com>
2025-02-26 17:26:19 -0700

    Using react-hook-forms to handle searches

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,38 @@
-import { HStack, Image, Input } from "@chakra-ui/react";
 import React from "react";
+import { HStack, Image, Input } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
 import { InputGroup } from "./ui/input-group";
 import { BsSearch } from "react-icons/bs";
-
 import GameHubSwitch from "./GameHubSwitch";
 import logo from "../assets/logo.webp";
+import { Field } from "./ui/field";
 
-function NavBar() {
+export interface SearchForm {
+  searchValue: string;
+}
+
+interface NavBarProps {
+  onSubmit: SubmitHandler<SearchForm>;
+}
+
+function NavBar({ onSubmit }: NavBarProps) {
+  const { register, handleSubmit } = useForm<SearchForm>();
+
   return (
     <HStack paddingX="100px">
       <Image src={logo} h="80px" />
-      <InputGroup flex="1" startElement={<BsSearch />}>
-        <Input placeholder="Search..." borderRadius="full" />
-      </InputGroup>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ flex: 1 }}>
+        <Field>
+          <InputGroup w="100%" startElement={<BsSearch />}>
+            <Input
+              {...register("searchValue")}
+              placeholder="Search..."
+              borderRadius="full"
+            />
+          </InputGroup>
+        </Field>
+      </form>
       <GameHubSwitch label="Dark Mode" />
     </HStack>
   );
