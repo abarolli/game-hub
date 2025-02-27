e3af0d53a81f217f0dcc2cf91720539a88dafc9a
abarolli <obarolli@gmail.com>
2025-02-26 22:32:27 -0700

    Shortened platform icon list

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { IconType } from "react-icons";
-import { Badge, Card, HStack, Image } from "@chakra-ui/react";
+import { Badge, Card, HStack, Image, Text } from "@chakra-ui/react";
 
 import { PlatformType } from "@/uiConfigs/platformIcons";
 import Entity from "./Entity";
@@ -19,18 +19,39 @@ export interface GameProps extends Entity {
 }
 
 function GameCard({ gameTitle, imgSrc, criticScore, platforms }: GameProps) {
-  const platformsAsIcons = platforms.map((platform) =>
-    platform.icon ? <platform.icon key={platform.id} /> : <LuGamepad />
-  );
+  const platformsAsIcons = () => {
+    const MAX_ICONS = 5;
+    const platformsAsIcons: JSX.Element[] = [];
+
+    for (let i = 0; i < Math.min(platforms.length, MAX_ICONS); i++) {
+      const platform = platforms[i];
+      platformsAsIcons.push(
+        platform.icon ? <platform.icon key={platform.id} /> : <LuGamepad />
+      );
+    }
+    if (platforms.length - MAX_ICONS > 0)
+      platformsAsIcons.push(<Text>+{platforms.length - MAX_ICONS}</Text>);
+
+    return platformsAsIcons;
+  };
+
+  const criticScoreBadgeColor = (score: number): string => {
+    if (score === undefined || score === null) return "gray";
+    return criticScore > 60 ? "green" : "red";
+  };
 
   return (
     <Card.Root rounded="xl">
       <Image h="xs" src={imgSrc} alt={gameTitle} roundedTop="xl" />
       <Card.Body>
         <HStack mb="20px" justifyContent="space-between">
-          <HStack>{platformsAsIcons}</HStack>
-          <Badge w="fit" paddingX="10px" colorPalette="green">
-            {criticScore}
+          <HStack>{platformsAsIcons()}</HStack>
+          <Badge
+            w="fit"
+            paddingX="10px"
+            colorPalette={criticScoreBadgeColor(criticScore)}
+          >
+            {criticScore ?? <Text>Score Unavailable</Text>}
           </Badge>
         </HStack>
         <Card.Title fontWeight="bolder" fontSize="1.5rem">
