a822905b23e88b00a9de2ca9ee56a9d46067ebc9
abarolli <obarolli@gmail.com>
2025-08-19 21:06:53 -0400

    Added currentGenre and currentPlatform state variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 import {
-  Box,
-  Flex,
-  SimpleGrid,
-  Heading,
-  GridItem,
-  createListCollection,
-  ListCollection,
+	Box,
+	Flex,
+	SimpleGrid,
+	Heading,
+	GridItem,
+	createListCollection,
+	ListCollection,
 } from "@chakra-ui/react";
 import { SubmitHandler } from "react-hook-form";
 
@@ -20,155 +20,227 @@ import Entity from "./components/Entity";
 import RawgHTTPService from "./service/rawgHttpService";
 import Selection, { SelectionInfo } from "./components/Selection";
 
+import "./index.css"
+
 interface RawgGame extends Entity {
-  name: string;
-  metacritic: number;
-  background_image: string;
-  parent_platforms?: { platform: Platform }[];
+	name: string;
+	metacritic: number;
+	background_image: string;
+	parent_platforms?: { platform: Platform }[];
 }
 
 interface RawgGenre extends Entity {
-  name: string;
-  image_background: string;
+	name: string;
+	image_background: string;
 }
 
 interface RawgPlatform extends Entity {
-  name: string;
+	name: string;
 }
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [games, setGames] = useState<GameProps[]>([]);
-  const [genres, setGenres] = useState<GenreProps[]>([]);
-  const [platformOptions, setPlatformOptions] =
-    useState<ListCollection<SelectionInfo>>();
+	const [searchTerm, setSearchTerm] = useState("");
+	const [games, setGames] = useState<GameProps[]>([]);
+	const [genres, setGenres] = useState<GenreProps[]>([]);
+	const [platformOptions, setPlatformOptions] =
+		useState<ListCollection<SelectionInfo>>();
 
-  const gamesHttpService = new RawgHTTPService<RawgGame>("/games", rawgClient);
-  const genresHttpService = new RawgHTTPService<RawgGenre>(
-    "/genres",
-    rawgClient
-  );
-  const platformsHttpService = new RawgHTTPService<RawgPlatform>(
-    "/platforms/lists/parents",
-    rawgClient
-  );
+	const gamesHttpService = new RawgHTTPService<RawgGame>("/games", rawgClient);
+	const genresHttpService = new RawgHTTPService<RawgGenre>(
+		"/genres",
+		rawgClient
+	);
+	const platformsHttpService = new RawgHTTPService<RawgPlatform>(
+		"/platforms/lists/parents",
+		rawgClient
+	);
 
-  const rawgGameToGameProps = (game: RawgGame): GameProps => {
-    return {
-      id: game.id,
-      gameTitle: game.name,
-      imgSrc: game.background_image,
-      criticScore: game.metacritic,
-      platforms:
-        game.parent_platforms?.map((p) => {
-          return {
-            id: p.platform.id,
-            name: p.platform.name,
-            icon: platformIcons[p.platform.name],
-          } as Platform;
-        }) || [],
-    };
-  };
+	const sentinelRef = useRef(null);
+	const page = useRef(2);
+	const [currentGenre, setCurrentGenre] = useState<number | null>(null);
+	const [currentPlatform, setCurrentPlatform] = useState<number | null>(null);
 
-  const updateGames = (games: RawgGame[]) => {
-    const newGames: GameProps[] = [];
-    for (let game of games) newGames.push(rawgGameToGameProps(game));
-    setGames(newGames);
-  };
+	const rawgGameToGameProps = (game: RawgGame): GameProps => {
+		return {
+			id: game.id,
+			gameTitle: game.name,
+			imgSrc: game.background_image,
+			criticScore: game.metacritic,
+			platforms:
+				game.parent_platforms?.map((p) => {
+					return {
+						id: p.platform.id,
+						name: p.platform.name,
+						icon: platformIcons[p.platform.name],
+					} as Platform;
+				}) || [],
+		};
+	};
 
-  const populateGames = () => {
-    gamesHttpService.getAll().then((games) => updateGames(games));
-  };
+	const updateGames = (games: RawgGame[]) => {
+		const newGames: GameProps[] = [];
+		for (let game of games) newGames.push(rawgGameToGameProps(game));
+		setGames(newGames);
+	};
 
-  const rawgGenreToGenreProps = (genre: RawgGenre): GenreProps => {
-    return { id: genre.id, label: genre.name, imgSrc: genre.image_background };
-  };
+	const appendGames = (games: RawgGame[]) => {
+		const newGames: GameProps[] = [];
+		for (let game of games) newGames.push(rawgGameToGameProps(game));
+		setGames((current) => [...current, ...newGames]);
+	}
 
-  const populateGenres = () => {
-    genresHttpService.getAll().then((genres) => {
-      const newGenres: GenreProps[] = [];
-      for (let genre of genres) newGenres.push(rawgGenreToGenreProps(genre));
-      setGenres(newGenres);
-    });
-  };
+	const populateGames = () => {
+		gamesHttpService.getAll().then((games) => updateGames(games));
+	};
 
-  const populatePlatformOptions = () => {
-    platformsHttpService.getAll().then((platforms) => {
-      setPlatformOptions(
-        createListCollection({
-          items: platforms.map((platform) => {
-            return {
-              id: platform.id,
-              label: platform.name,
-              value: platform.name,
-            } as SelectionInfo;
-          }),
-        })
-      );
-    });
-  };
+	const rawgGenreToGenreProps = (genre: RawgGenre): GenreProps => {
+		return { id: genre.id, label: genre.name, imgSrc: genre.image_background };
+	};
 
-  useEffect(populateGames, []);
-  useEffect(populateGenres, []);
-  useEffect(populatePlatformOptions, []);
+	const populateGenres = () => {
+		genresHttpService.getAll().then((genres) => {
+			const newGenres: GenreProps[] = [];
+			for (let genre of genres) newGenres.push(rawgGenreToGenreProps(genre));
+			setGenres(newGenres);
+		});
+	};
 
-  const searchHandler: SubmitHandler<SearchForm> = (data) => {
-    gamesHttpService.find({ search: data.searchValue }).then((games) => {
-      updateGames(games);
-      setSearchTerm(data.searchValue);
-    });
-  };
+	const populatePlatformOptions = () => {
+		platformsHttpService.getAll().then((platforms) => {
+			setPlatformOptions(
+				createListCollection({
+					items: platforms.map((platform) => {
+						return {
+							id: platform.id,
+							label: platform.name,
+							value: platform.name,
+						} as SelectionInfo;
+					}),
+				})
+			);
+		});
+	};
 
-  const platformSelectHandler = (id: number) => {
-    gamesHttpService
-      .find({ parent_platforms: id })
-      .then((games) => updateGames(games));
-  };
+	const loadMoreGames = useCallback(() => {
+		console.log("Loading more games");
+		gamesHttpService
+			.find({ page: page.current, genres: currentGenre, parent_platforms: currentPlatform })
+			.then((games) => {
+				appendGames(games);
+				page.current += 1;
+			});
+	}, [currentGenre, currentPlatform]);
 
-  const genreClickHandler = (id: number) => {
-    gamesHttpService.find({ genres: id }).then((games) => updateGames(games));
-  };
+	useEffect(populateGames, []);
+	useEffect(populateGenres, []);
+	useEffect(populatePlatformOptions, []);
 
-  return (
-    <>
-      <Box mb="30px">
-        <NavBar onSubmit={searchHandler} />
-      </Box>
-      <Flex justifyContent="center">
-        <Flex maxW="1500px" w="100%">
-          <Box w="300px" p="20px">
-            <Heading mb="30px">Genres</Heading>
-            <GenreList genres={genres} onClick={genreClickHandler} />
-          </Box>
-          <SimpleGrid
-            minChildWidth="xs"
-            columnGap="20px"
-            rowGap="20px"
-            padding="20px"
-            flex="1"
-          >
-            <GridItem gridColumn="1 / -1">
-              <Heading mb="20px" fontSize="2rem" fontWeight="bolder">
-                {searchTerm || "Games"}
-              </Heading>
-              <Box w="175px">
-                {platformOptions && (
-                  <Selection
-                    onSelect={platformSelectHandler}
-                    collection={platformOptions}
-                    placeholder="Platforms"
-                  />
-                )}
-              </Box>
-            </GridItem>
-            {games.map((game) => {
-              return <GameCard key={game.id} {...game} />;
-            })}
-          </SimpleGrid>
-        </Flex>
-      </Flex>
-    </>
-  );
+	useEffect(() => {
+
+		const sentinel = sentinelRef.current;
+		if (sentinel === null) return;
+
+		console.log("Creating observer");
+
+		const observer = createObserver(sentinel);
+
+		return () => observer.disconnect();
+
+	}, [loadMoreGames]);
+
+	const searchHandler: SubmitHandler<SearchForm> = (data) => {
+		gamesHttpService.find({ search: data.searchValue }).then((games) => {
+			updateGames(games);
+			setSearchTerm(data.searchValue);
+			page.current = 2;
+		});
+	};
+
+	const platformSelectHandler = (id: number) => {
+		gamesHttpService
+			.find({ genres: currentGenre, parent_platforms: id })
+			.then((games) => {
+				updateGames(games);
+				setCurrentPlatform(id);
+				page.current = 2;
+			});
+
+	};
+
+	const genreClickHandler = (id: number) => {
+		gamesHttpService
+			.find({ genres: id, parent_platforms: currentPlatform })
+			.then((games) => {
+				updateGames(games);
+				setCurrentGenre(id);
+				page.current = 2;
+			});
+
+	};
+
+
+
+	const intersectionHandler = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+		let entry = entries[0];
+		if (entry && entry.isIntersecting) {
+			loadMoreGames();
+		}
+	}
+
+	const createObserver = (target: any): IntersectionObserver => {
+
+		const observer = new IntersectionObserver(intersectionHandler, {
+			root: null,
+			rootMargin: "200px",
+			threshold: 0
+		});
+
+		observer.observe(target);
+
+		return observer;
+	}
+
+	return (
+		<>
+			<Box mb="30px">
+				<NavBar onSubmit={searchHandler} />
+			</Box>
+			<Flex justifyContent="center">
+				<Flex maxW="1500px" w="100%">
+					<Box w="300px" p="20px">
+						<Heading mb="30px">Genres</Heading>
+						<GenreList selected={currentGenre} genres={genres} onClick={genreClickHandler} />
+					</Box>
+					<SimpleGrid
+						minChildWidth="xs"
+						columnGap="20px"
+						rowGap="20px"
+						padding="20px"
+						flex="1"
+					>
+						<GridItem gridColumn="1 / -1">
+							<Heading mb="20px" fontSize="2rem" fontWeight="bolder">
+								{searchTerm || "Games"}
+							</Heading>
+							<Box w="175px">
+								{platformOptions && (
+									<Selection
+										onSelect={platformSelectHandler}
+										collection={platformOptions}
+										placeholder="Platforms"
+									/>
+								)}
+							</Box>
+						</GridItem>
+						{games.map((game) => {
+							return <GameCard key={game.id} {...game} />;
+						})}
+					</SimpleGrid>
+				</Flex>
+			</Flex>
+			<div ref={sentinelRef} style={{ height: 1 }} />
+		</>
+	);
 }
 
 export default App;
